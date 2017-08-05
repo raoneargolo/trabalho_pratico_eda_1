71362f449d1ca49f203440334c734f4de0c7b850
Unknown <erick.gtn.xp@gmail.com>
2017-08-05 18:58:08 -0300

    Criação das funções de busca pela matricula e pelo nome

--- Arvore.c
+++ Arvore.c
@@ -12,13 +12,92 @@ typedef struct celulaAVL {
 	struct celula_AVL *filho_dir;
 }celula_AVL,*RegistroAVL;
 
+typedef celula_AVL celula_AVL;
+typedef celula_AVL *celula_AVL_ptr;
+
 
 //Funcao de menu
 //Funcao de inserção na ListaDUP
+
 //Funcao de inserção na ArvoreAVL
-	//Funcao de Criacao de arvore
+void insere_AVL (int matricula, char *nome, char *sobrenome, char *email, char *telefone, double salario, celula_AVL_ptr* arvore_AVL) 
+{
+	if (arvore_AVL == NULL) //quando a inserção for na folha
+
+	{
+		arvore_AVL = (celula_AVL_ptr*)malloc(sizeof(celula_AVL_ptr)) 
+		arvore_AVL-> matricula=matricula;
+		Strcpy(arvore_AVL-> nome,nome);
+		Strcpy(arvore_AVL->sobrenome,sobrenome);
+		Strcpy(arvore_AVL->email,email);
+		Strcpy(arvore_AVL->telefone,telefone);
+		arvore_AVL->salario=salario;
+		arvore_AVL->filho_esq = NULL;
+		arvore_AVL->filho_dir = NULL;
+
+	}
+
+	elseif (matricula < arvore_AVL->matricula)
+		arvore_AVL-> filho_esq = insere_AVL(matricula, nome, sobrenome, email, telefone, salario, arvore_AVL->filho_esq);
+	else
+		arvore_AVL->filho_dir = insere_AVL(matricula, nome, sobrenome, email, telefone, salario, arvore_AVL->filho_dir);
+
+}
+
+
+
+//Funcao de Criacao de arvore
+void celula_AVL_ptr *cria_arvore_AVL
+{
+	return NULL;
+}
+
 	//Funcao de inserção de um elemento na Arvore
-	//Funcao de busca na arvore
+//Funcao de busca matricula na arvore
+int busca_matricula(celula_AVL *arvore, int matricula)
+{
+
+	if (arvore != NULL) {
+		if arvore->matricula == matricula) {
+			printf(" matricula %d encontrada", matricula);
+			printf("nome:%s\n", arvore ->nome);
+			return 1; //Achou o elemento
+		}
+	elseif (arvore->matricula > matricula) 
+	{
+		busca_matricula (arvore->filho_esq, matricula);
+	}
+	else 
+	{
+		busca_matricula (arvore->filho_dir,matricula);
+	}		
+}
+	return 0; //não achou o elemento
+	
+
+}
+
+int busca_nome(celula_AVL *arvore, char *nome, char *sobrenome)
+{
+
+	if (arvore != NULL) {
+		if (strcmp (arvore->nome,nome) == 0) && (strcmp arvore->sobrenome,sobrenome) == 0)
+		{
+			printf(" nome %s%s encontrado", nome,sobrenome);
+			return 1; //Achou o elemento
+		}
+	
+		busca_matricula (arvore->filho_esq, nome,sobrenome);
+	
+		busca_matricula (arvore->filho_dir,nome,sobrenome);
+	}		
+}
+	return 0; //não achou o elemento
+	
+}
+
+
+
 	//
 //Funcao de remocao de ListaDUP
 //Funcaode remocao de ArvoreAVL
