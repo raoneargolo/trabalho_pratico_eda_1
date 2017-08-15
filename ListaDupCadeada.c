a7606f6bdc2899ff0e839031229436cad9c3c8bf
Unknown <erick.gtn.xp@gmail.com>
2017-08-15 18:09:28 -0300

    Criação das funções de Inserção e Busca

--- ListaDupCadeada.c
+++ ListaDupCadeada.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 //Criação da estrutura ListaDUP.............................OK
 //Funcao de Criacao de lista................................PENDENTE
@@ -22,17 +23,16 @@ struct celula_DUP {
 	struct celula_DUP *anterior;
 };
 
-
 typedef struct celula_DUP celula_DUP;
 typedef celula_DUP *celula_DUP_ptr;
 
 //Funcao de insercao
 
 void insere (int matricula, char *nome, char *sobrenome, char *email, char *telefone, double salario, celula_DUP_ptr *lista_DUP){
-
+celula_DUP_ptr novo;
 if (lista_DUP == NULL) //quando a inserção for no inicio da lista
 	{
-		celula_DUP_ptr novo = ((celula_DUP_ptr*)malloc(sizeof(celula_DUP_ptr)));
+		novo = ((celula_DUP_ptr)malloc(sizeof(celula_DUP_ptr)));
 		novo->matricula=matricula;
 		strcpy(novo-> nome,nome);
 		strcpy(novo->sobrenome,sobrenome);
@@ -41,9 +41,32 @@ if (lista_DUP == NULL) //quando a inserção for no inicio da lista
 		novo->salario=salario;
 		novo->proximo = NULL;
 		novo->anterior = NULL;
-		if (Lista_DUP !=NULL)
-			lista_DUP->proximo = novo;
 		//return novo;
 	}
-	
+	(*lista_DUP)->proximo = novo;
+}
+
+//FUNCAO DE BUSCA POR MATRICULA
+celula_DUP_ptr Busca_Matricula (celula_DUP_ptr *lista_DUP, int matricula)
+ {
+	celula_DUP_ptr p;
+	for(p=*lista_DUP; p!=NULL;p=p->proximo){
+			if(p->matricula == matricula)
+				return p;
+	}
+	return NULL;
+ }
+
+
+//FUNCAO DE REMOCAO DE ELEMENTO
+celula_DUP_ptr Remove_Elemento (celula_DUP_ptr *lista_DUP,int matricula)
+{
+	celula_DUP_ptr a=NULL;
+	celula_DUP_ptr p=*lista_DUP;
+	while(p!=NULL && p->matricula !=matricula)
+	{
+		a=p;
+		p=p->proximo;
+	}
+	return *lista_DUP;
 }
